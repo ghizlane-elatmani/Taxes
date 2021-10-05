a532cc659655506cd47027da2a673b80bf20e037
riri <ghizlane.elatmani.99@gmail.com>
2021-10-05 10:38:34 +0100

    1.7.0 - Add pagination for entreprises'List

--- src/main/java/io/javacode/taxes/web/TaxeController.java
+++ src/main/java/io/javacode/taxes/web/TaxeController.java
@@ -3,10 +3,13 @@ package io.javacode.taxes.web;
 import io.javacode.taxes.dao.EntrepriseRepository;
 import io.javacode.taxes.entities.Entreprise;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,9 +20,14 @@ public class TaxeController {
     private EntrepriseRepository entrepriseRepository;
 
     @RequestMapping(value = "/entreprises", method = RequestMethod.GET)
-    public String index(Model model){
-        List<Entreprise> entrepriseList = entrepriseRepository.findAll();
-        model.addAttribute("entrepriseList", entrepriseList);
+    public String index(Model model,
+                        @RequestParam(name="page", defaultValue="0") int page,
+                        @RequestParam(name="size", defaultValue="4") int size){
+        Page<Entreprise> entreprisesPage = entrepriseRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("entrepriseList", entreprisesPage.getContent());
+
+        int[] pages = new int[entreprisesPage.getTotalPages()];
+        model.addAttribute("pages", pages);
         return "entreprises";
     }
 
