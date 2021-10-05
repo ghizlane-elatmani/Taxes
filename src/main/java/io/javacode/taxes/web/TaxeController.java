bdb25e59288cb1c330bb321d5845f295e993ea8c
riri <ghizlane.elatmani.99@gmail.com>
2021-10-05 08:12:37 +0100

    1.5.1 - Display entreprise'list

--- src/main/java/io/javacode/taxes/web/TaxeController.java
+++ src/main/java/io/javacode/taxes/web/TaxeController.java
@@ -1,10 +1,14 @@
 package io.javacode.taxes.web;
 
 import io.javacode.taxes.dao.EntrepriseRepository;
+import io.javacode.taxes.entities.Entreprise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class TaxeController {
@@ -13,7 +17,9 @@ public class TaxeController {
     private EntrepriseRepository entrepriseRepository;
 
     @RequestMapping(value = "/entreprises", method = RequestMethod.GET)
-    public String index(){
+    public String index(Model model){
+        List<Entreprise> entrepriseList = entrepriseRepository.findAll();
+        model.addAttribute("entrepriseList", entrepriseList);
         return "entreprises";
     }
 
