fe0198882ff71f87a2e1c54db8338b13ed2547c9
riri <ghizlane.elatmani.99@gmail.com>
2021-10-07 19:50:08 +0100

    1.16.0 - Styling login'form

--- src/main/java/io/javacode/taxes/security/SecurityConfig.java
+++ src/main/java/io/javacode/taxes/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
+        http.formLogin().loginPage("/login");
 
         /*
         * This line mean that every request needs an authentication
---
+++ src/main/java/io/javacode/taxes/security/SecurityController.java
@@ -0,0 +1,24 @@
+package io.javacode.taxes.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecurityController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/")
+    public String home(){
+        return "redirect:/entreprises";
+    }
+
+    @RequestMapping("/403")
+    public String accessDenied(){
+        return "403";
+    }
+
+}
