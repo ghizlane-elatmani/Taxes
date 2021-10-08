b7059e53864c5ef4b5b7c6b629f645b4c058cc1f
riri <ghizlane.elatmani.99@gmail.com>
2021-10-08 17:29:44 +0100

    1.18.0 - Spring Security with database

--- src/main/java/io/javacode/taxes/security/SecurityConfig.java
+++ src/main/java/io/javacode/taxes/security/SecurityConfig.java
@@ -1,17 +1,27 @@
 package io.javacode.taxes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*
         auth.inMemoryAuthentication()
                 .withUser("hassan")
                 .password("{noop}123")
@@ -21,6 +31,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("mohamed")
                 .password("{noop}123")
                 .roles("USER");
+        */
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, pass as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder()); ;
+    }
+
+    // This is just for educational reason. Do not use it on a real project!
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
