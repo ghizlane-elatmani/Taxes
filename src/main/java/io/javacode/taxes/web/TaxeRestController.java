017f7b550df686c4bf3fdcd1163bff79ac0e5937
riri <ghizlane.elatmani.99@gmail.com>
2021-10-09 13:00:04 +0100

    1.20.0 - Starting client side

--- src/main/java/io/javacode/taxes/web/TaxeRestController.java
+++ src/main/java/io/javacode/taxes/web/TaxeRestController.java
@@ -3,7 +3,10 @@ package io.javacode.taxes.web;
 import io.javacode.taxes.dao.EntrepriseRepository;
 import io.javacode.taxes.entities.Entreprise;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +18,11 @@ public class TaxeRestController {
     private EntrepriseRepository entrepriseRepository;
 
     @RequestMapping("/listEntreprises")
-    public List<Entreprise> list(){
-        return entrepriseRepository.findAll();
+    public Page<Entreprise> list(
+            @RequestParam(name = "motCle", defaultValue = "") String motCle,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "4") int size){
+        return entrepriseRepository.chercher("%"+motCle+"%", PageRequest.of(page, size));
     }
 
 }
